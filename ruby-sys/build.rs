b5680084abfa93c85e9f57ab1189c11c7494377e
Nikolai Vazquez <nikvzqz@gmail.com>
2019-05-12 19:18:44 +0200

    Add support for building Rosy via `rvm` or `rbenv`
    Also reorganized the build script for `ruby-sys`.

--- ruby-sys/build.rs
+++ ruby-sys/build.rs
@@ -1,80 +1,43 @@
 extern crate aloxide;
 extern crate bindgen;
 
-use std::env;
-use std::fmt::Display;
-use std::fs::File;
-use std::io::Write;
-use std::path::{Path, PathBuf};
+use std::{
+    env,
+    fmt::Display,
+    path::PathBuf,
+};
+use aloxide::Ruby;
 
-use aloxide::{Ruby, Version};
+#[path = "build/bindings.rs"]
+mod bindings;
+#[path = "build/ruby.rs"]
+mod ruby;
 
 const LINK_STATIC: bool = cfg!(feature = "static");
-
-fn generate_bindings(ruby: &Ruby, path: &Path) {
-    let header = ruby.wrapper_header().expect("Generate header");
-    let header_dir = ruby.header_dir().expect("Get header dir");
-    let arch_header_dir = ruby.arch_header_dir().expect("Get arch header dir");
-
-    bindgen::builder()
-        .header_contents("rosy.h", &header)
-        .clang_args(&[
-            format!("-I{}", header_dir),
-            format!("-I{}", arch_header_dir),
-        ])
-        .default_enum_style(bindgen::EnumVariation::ModuleConsts)
-        .rustified_enum("ruby_value_type")
-        .generate()
-        .expect("Generate bindings")
-        .write_to_file(path)
-        .expect(&format!("Could not write bindings to {:?}", path));
-}
 
 fn set_rustc_env(var: impl Display, val: impl Display) {
     println!("cargo:rustc-env={}={}", var, val);
 }
 
-fn write_ruby_version_const(version: &Version, out_dir: &Path) {
-    let path = out_dir.join("ruby_version.rs");
-    set_rustc_env("ROSY_RUBY_VERSION_CONST", path.display());
-
-    let mut file = File::create(&path).expect("Create Ruby version file");
-    write!(
-        file,
-        "/// The version of the Ruby library API being used: **{v}**.\n\
-        ///\n\
-        /// Note that this may differ from the version of the actual Ruby\n\
-        /// library being linked to when using dynamic linking.\n\
-        pub const RUBY_VERSION: &str = \"{v}\";",
-        v = version,
-    ).expect("Write Ruby version");
+fn rerun_if_env_changed(var: impl Display) {
+    println!("cargo:rerun-if-env-changed={}", var);
 }
 
-#[cfg(feature = "download")]
-fn ruby() -> Ruby {
-    unimplemented!()
-}
-
-#[cfg(not(feature = "download"))]
-fn ruby() -> Ruby {
-    Ruby::current().unwrap()
+fn print_config(ruby: &Ruby) {
+    rerun_if_env_changed("ROSY_PRINT_RUBY_CONFIG");
+    if env::var_os("ROSY_PRINT_RUBY_CONFIG").is_some() {
+        println!("{}", ruby.run("require 'pp'; pp RbConfig::CONFIG").unwrap());
+    }
 }
 
 fn main() {
-    let ruby = ruby();
-    println!("{}", ruby.run("require 'pp'; pp RbConfig::CONFIG").unwrap());
+    let ruby = ruby::get();
+    print_config(&ruby);
     ruby.link(LINK_STATIC).unwrap();
 
     let out_dir = env::var_os("OUT_DIR").expect("Couldn't get 'OUT_DIR'");
     let out_dir = PathBuf::from(out_dir);
 
-    write_ruby_version_const(ruby.version(), &out_dir);
-
-    let bindings_name = format!("ruby-{}.rs", ruby.version());
-    let bindings_path = out_dir.join(&bindings_name);
-    set_rustc_env("ROSY_BINDINGS_PATH", bindings_path.display());
-
-    if !bindings_path.exists() || env::var_os("ROSY_BINDGEN").is_some() {
-        generate_bindings(&ruby, &bindings_path);
-    }
+    ruby::write_version_const(ruby.version(), &out_dir);
+    bindings::write(&ruby, &out_dir);
 }
---
+++ ruby-sys/build/ruby.rs
@@ -0,0 +1,121 @@
+use std::{
+    env,
+    fs::File,
+    ffi::OsStr,
+    io::Write,
+    path::{Path, PathBuf},
+};
+use aloxide::{Ruby, Version};
+
+// An external tool that manages the Ruby installation
+enum Manager {
+    // Download and build Ruby ourselves
+    Download,
+    // https://www.github.com/rbenv/rbenv
+    Rbenv,
+    // https://github.com/rvm/rvm
+    Rvm,
+}
+
+impl Manager {
+    fn from_var(var: &OsStr) -> Option<Self> {
+        if var == "download" {
+            Some(Manager::Download)
+        } else if var == "rbenv" {
+            Some(Manager::Rbenv)
+        } else if var == "rvm" {
+            Some(Manager::Rvm)
+        } else {
+            None
+        }
+    }
+
+    fn ruby(self, v: &Version) -> Ruby {
+        match self {
+            Manager::Rbenv => {
+                Ruby::from_rbenv(v).expect("Could not get Ruby from 'rbenv'")
+            },
+            Manager::Rvm => {
+                Ruby::from_rvm(v).expect("Could not get Ruby from 'rvm'")
+            },
+            Manager::Download => download(),
+        }
+    }
+}
+
+enum Driver {
+    Manager(Manager),
+    // A path to a Ruby executable
+    Path(PathBuf),
+}
+
+impl Driver {
+    fn get() -> Option<Self> {
+        let ruby = env::var_os("ROSY_RUBY")?;
+        if ruby.is_empty() {
+            None
+        } else if let Some(manager) = Manager::from_var(&ruby) {
+            Some(Driver::Manager(manager))
+        } else {
+            Some(Driver::Path(ruby.into()))
+        }
+    }
+
+    fn ruby(self) -> Ruby {
+        match self {
+            Driver::Manager(manager) => {
+                manager.ruby(&version().expect("'ROSY_RUBY_VERSION' not set"))
+            },
+            Driver::Path(ruby) => {
+                Ruby::from_bin(&ruby)
+                    .expect(&format!(
+                        "Could not get Ruby from '{}'",
+                        ruby.display(),
+                    ))
+            }
+        }
+    }
+}
+
+fn version() -> Option<Version> {
+    super::rerun_if_env_changed("ROSY_RUBY_VERSION");
+    Some(env::var_os("ROSY_RUBY_VERSION")?
+        .to_str()
+        .expect("'ROSY_RUBY_VERSION' is not UTF-8")
+        .parse()
+        .expect("Could not parse 'ROSY_RUBY_VERSION'"))
+}
+
+pub fn write_version_const(version: &Version, out_dir: &Path) {
+    let path = out_dir.join("ruby_version.rs");
+    super::set_rustc_env("ROSY_RUBY_VERSION_CONST", path.display());
+
+    let mut file = File::create(&path).expect("Create Ruby version file");
+    write!(
+        file,
+        "/// The version of the Ruby library API being used: **{v}**.\n\
+        ///\n\
+        /// Note that this may differ from the version of the actual Ruby\n\
+        /// library being linked to when using dynamic linking.\n\
+        pub const RUBY_VERSION: &str = \"{v}\";",
+        v = version,
+    ).expect("Could not write `RUBY_VERSION` const");
+}
+
+#[cfg(feature = "download")]
+fn download() -> Ruby {
+    unimplemented!("Can't download yet")
+}
+
+#[cfg(not(feature = "download"))]
+fn download() -> Ruby {
+    panic!("Enable 'download' feature in 'Cargo.toml' to download Ruby");
+}
+
+pub fn get() -> Ruby {
+    if let Some(driver) = Driver::get() {
+        driver.ruby()
+    } else {
+        Ruby::current().expect("Could not get system Ruby in 'PATH'")
+    }
+}
