4c264fcb02b74e09a021ef2c7ff3a9f42501eca2
Nikolai Vazquez <nikvzqz@gmail.com>
2019-05-21 17:01:04 +0200

    Add `convert_c_str` convenience function to `meta`

--- src/meta.rs
+++ src/meta.rs
@@ -6,6 +6,16 @@ use std::{
 };
 use crate::ruby;
 
+#[inline]
+unsafe fn convert_c_str<'a>(s: &'a CStr, debug_message: &str) -> &'a str {
+    let bytes = s.to_bytes();
+    if cfg!(debug_assertions) {
+        str::from_utf8(bytes).expect(debug_message)
+    } else {
+        str::from_utf8_unchecked(bytes)
+    }
+}
+
 /// Ruby's API version.
 ///
 /// Note that this may differ from the result of `version_str`.
@@ -18,12 +28,7 @@ pub fn api_version() -> (u16, u16, u16) {
 /// Ruby's version info as a UTF-8 string.
 #[inline]
 pub fn version_str<'a>() -> &'a str {
-    let bytes = version_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby version is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
-    }
+    unsafe { convert_c_str(version_c_str(), "Ruby version is not UTF-8") }
 }
 
 /// Ruby's version info as a C string.
@@ -35,11 +40,8 @@ pub fn version_c_str<'a>() -> &'a CStr {
 /// Ruby's release date info as a UTF-8 string.
 #[inline]
 pub fn release_date_str<'a>() -> &'a str {
-    let bytes = release_date_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby release date is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
+    unsafe {
+        convert_c_str(release_date_c_str(), "Ruby release date is not UTF-8")
     }
 }
 
@@ -52,12 +54,7 @@ pub fn release_date_c_str<'a>() -> &'a CStr {
 /// Ruby's platform info as a UTF-8 string.
 #[inline]
 pub fn platform_str<'a>() -> &'a str {
-    let bytes = platform_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby platform is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
-    }
+    unsafe { convert_c_str(platform_c_str(), "Ruby platform is not UTF-8") }
 }
 
 /// Ruby's platform info as a C string.
@@ -69,11 +66,8 @@ pub fn platform_c_str<'a>() -> &'a CStr {
 /// Ruby's description info as a UTF-8 string.
 #[inline]
 pub fn description_str<'a>() -> &'a str {
-    let bytes = description_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby description is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
+    unsafe {
+        convert_c_str(description_c_str(), "Ruby description is not UTF-8")
     }
 }
 
@@ -86,12 +80,7 @@ pub fn description_c_str<'a>() -> &'a CStr {
 /// Ruby's copyright info as a UTF-8 string.
 #[inline]
 pub fn copyright_str<'a>() -> &'a str {
-    let bytes = copyright_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby copyright is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
-    }
+    unsafe { convert_c_str(copyright_c_str(), "Ruby copyright is not UTF-8") }
 }
 
 /// Ruby's copyright info as a C string.
@@ -103,12 +92,7 @@ pub fn copyright_c_str<'a>() -> &'a CStr {
 /// Ruby's engine info as a UTF-8 string.
 #[inline]
 pub fn engine_str<'a>() -> &'a str {
-    let bytes = engine_c_str().to_bytes();
-    if cfg!(debug_assertions) {
-        str::from_utf8(bytes).expect("Ruby engine is not UTF-8")
-    } else {
-        unsafe { str::from_utf8_unchecked(bytes) }
-    }
+    unsafe { convert_c_str(engine_c_str(), "Ruby engine is not UTF-8") }
 }
 
 /// Ruby's engine info as a C string.
